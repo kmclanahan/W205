a63c519286123e818e2fdce237758f8edee7bf65
kmclanahan <kmclanahan@ischool.berkeley.edu>
2016-02-03 20:12:03 -0700

    Update load_data_lake.sh

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -7,12 +7,16 @@ tail -n +2 ./'Readmissions and Deaths - Hospital.csv' > ./hospital/readmissions.
 tail -n +2 ./'Measure Dates.csv' > ./hospital/measures.csv
 tail -n +2 ./hvbp_hcahps_05_28_2015.csv > ./hospital/survey_responses.csv
 
-hdfs dfs -rm /user/w205/hospital_compare/*
-hdfs dfs -rmdir /user/w205/hospital_compare
+hdfs dfs -rm -r /user/w205/hospital_compare
 hdfs dfs -mkdir /user/w205/hospital_compare
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/measures
+hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
 
-hdfs dfs -put ./hospital/hospitals.csv /user/w205/hospital_compare
-hdfs dfs -put ./hospital/effective_care.csv /user/w205/hospital_compare
-hdfs dfs -put ./hospital/readmissions.csv /user/w205/hospital_compare
-hdfs dfs -put ./hospital/measures.csv /user/w205/hospital_compare
-hdfs dfs -put ./hospital/survey_responses.csv /user/w205/hospital_compare
+hdfs dfs -put ./hospital/hospitals.csv /user/w205/hospital_compare/hospitals
+hdfs dfs -put ./hospital/effective_care.csv /user/w205/hospital_compare/effective_care
+hdfs dfs -put ./hospital/readmissions.csv /user/w205/hospital_compare/readmissions
+hdfs dfs -put ./hospital/measures.csv /user/w205/hospital_compare/measures
+hdfs dfs -put ./hospital/survey_responses.csv /user/w205/hospital_compare/survey_responses
