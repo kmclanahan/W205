4302c8847f74881fb498bafeddd53940c0997080
kmclanahan <kmclanahan@ischool.berkeley.edu>
2016-02-14 10:56:45 -0700

    Change to explicit paths

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -1,11 +1,11 @@
-rm -rf ./hospital
-mkdir ./hospital
+rm -rf /home/w205/hospital
+mkdir /home/w205/hospital
 
-tail -n +2 ./'Hospital General Information.csv' > ./hospital/hospitals.csv
-tail -n +2 ./'Timely and Effective Care - Hospital.csv' > ./hospital/effective_care.csv
-tail -n +2 ./'Readmissions and Deaths - Hospital.csv' > ./hospital/readmissions.csv
-tail -n +2 ./'Measure Dates.csv' > ./hospital/measures.csv
-tail -n +2 ./hvbp_hcahps_05_28_2015.csv > ./hospital/survey_responses.csv
+tail -n +2 /home/w205/'Hospital General Information.csv' > /home/w205/hospital/hospitals.csv
+tail -n +2 /home/w205/'Timely and Effective Care - Hospital.csv' > /home/w205/hospital/effective_care.csv
+tail -n +2 /home/w205/'Readmissions and Deaths - Hospital.csv' > /home/w205/hospital/readmissions.csv
+tail -n +2 /home/w205/'Measure Dates.csv' > /home/w205/hospital/measures.csv
+tail -n +2 /home/w205/hvbp_hcahps_05_28_2015.csv > /home/w205/hospital/survey_responses.csv
 
 hdfs dfs -rm -r /user/w205/hospital_compare
 hdfs dfs -mkdir /user/w205/hospital_compare
@@ -15,8 +15,8 @@ hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
 hdfs dfs -mkdir /user/w205/hospital_compare/measures
 hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
 
-hdfs dfs -put ./hospital/hospitals.csv /user/w205/hospital_compare/hospitals
-hdfs dfs -put ./hospital/effective_care.csv /user/w205/hospital_compare/effective_care
-hdfs dfs -put ./hospital/readmissions.csv /user/w205/hospital_compare/readmissions
-hdfs dfs -put ./hospital/measures.csv /user/w205/hospital_compare/measures
-hdfs dfs -put ./hospital/survey_responses.csv /user/w205/hospital_compare/survey_responses
+hdfs dfs -put /home/w205/hospital/hospitals.csv /user/w205/hospital_compare/hospitals
+hdfs dfs -put /home/w205/hospital/effective_care.csv /user/w205/hospital_compare/effective_care
+hdfs dfs -put /home/w205/hospital/readmissions.csv /user/w205/hospital_compare/readmissions
+hdfs dfs -put /home/w205/hospital/measures.csv /user/w205/hospital_compare/measures
+hdfs dfs -put /home/w205/hospital/survey_responses.csv /user/w205/hospital_compare/survey_responses
