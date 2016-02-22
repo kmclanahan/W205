888082c3893b92a63867ea25920fdc0c4e6fa030
kmclanahan <kmclanahan@ischool.berkeley.edu>
2016-02-21 17:00:06 -0700

    Update survey_response.sql

--- exercise_1/investigations/survey_response.sql
+++ exercise_1/investigations/survey_response.sql
@@ -1,7 +1,9 @@
-SELECT h.name, s.hcahps_base_score
+SELECT h.name, AVG(s.hcahps_base_score) AS avg_score,
+COUNT(s.hcahps_base_score) AS num_surveys
 FROM survey_response_data s 
 JOIN hospital_data h
 ON s.provider_id = h.provider_id
-WHERE s.hcahps_base_score NOT LIKE "%Not Available%"
-ORDER BY s.hcahps_base_score DESC
+GROUP BY h.name
+ORDER BY avg_score DESC
 LIMIT 20;
+
