eb4bf04fd4c73a59d4d73a10e53831ff6fff055b
kmclanahan <kmclanahan@ischool.berkeley.edu>
2016-02-26 11:02:06 -0700

    Update survey_response.sql

--- exercise_1/investigations/survey_response.sql
+++ exercise_1/investigations/survey_response.sql
@@ -1,9 +1,12 @@
-SELECT h.name, AVG(s.hcahps_base_score) AS avg_score,
-COUNT(s.hcahps_base_score) AS num_surveys
-FROM survey_response_data s 
+SELECT h.name, SUM(e.score) AS cumulative_score, 
+AVG(e.score) as avg_score,
+VARIANCE(e.score) as variance,
+AVG(s.hcahps_base_score)
+FROM effective_care_data e
 JOIN hospital_data h
+ON e.provider_id = h.provider_id
+JOIN survey_response_data s 
 ON s.provider_id = h.provider_id
 GROUP BY h.name
 ORDER BY avg_score DESC
-LIMIT 20;
-
+LIMIT 50;
